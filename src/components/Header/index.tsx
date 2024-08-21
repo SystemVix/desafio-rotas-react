e0510b8e4093e98208cc3075ed7ed51706f8750a
Herhos <herhos@outlook.com>
2024-08-21 13:38:45 -0300

    Componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,31 @@
+import { NavLink } from 'react-router-dom';
 import './styles.css';
 
 export default function Header()
 {
    return (
-      Header
+      <>
+         <header>
+            <div className="conteiner-global conteiner-header">
+               <nav className="opcoes-header">
+                  <NavLink to="/home" className={({isActive}) =>
+                     isActive ? "opcao-item menu-ativo" : "opcao-item"}>
+                     Início
+                  </NavLink>
+                  <NavLink to="/products" className={({isActive}) =>
+                     isActive ? "opcao-item menu-ativo" : "opcao-item"}>
+                     Produtos
+                  </NavLink>
+                  <NavLink to="/about" className={({isActive}) =>
+                     isActive ? "opcao-item menu-ativo" : "opcao-item"}>
+                     Sobre
+                  </NavLink>
+               </nav>
+               <div>
+                  <img className="imagem" src="./src/assets/home.svg" alt="Home" />
+               </div>
+            </div>
+         </header>         
+      </>
    );
 }
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,6 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Header from "../../components/Header";
+
 export default function Home()
 {
    return (
-      Home
+      <>
+         <Header/>
+         <Outlet/>
+      </>
    );
 }
